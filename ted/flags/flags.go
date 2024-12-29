993c6cd9a7eaa7746a34c09d67c66d7da0eb66b0
Armand Halbert <armand.halbert@gmail.com>
2024-12-29 09:46:03 -0500

    Added support for non-newline record seperators (#57)

--- ted/flags/flags.go
+++ ted/flags/flags.go
@@ -3,6 +3,7 @@ package flags
 var Flags struct {
 	ProgramFile string   `arg:"-f,--fsa-file" placeholder:"FSAFILE" help:"Finite State Autonoma file to run."`
 	NoPrint     bool     `arg:"-n,--no-print" help:"Do not print lines by default."`
+	Seperator   string   `arg:"-s,--seperator" help:"Record Seperator. Defaults to \\n"`
 	DebugMode   bool     `arg:"--debug" help:"Provides Lexer and Parser information."`
 	Variables   []string `arg:"--var,separate" placeholder:"key=value" help:"Variable in the format name=value."`
 	Program     string   `arg:"positional" help:"Program to run."`
